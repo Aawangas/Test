b0aa8808c3ee00886fd7865968a5f1fba6a3bd07
Aawangas <145684420+Aawangas@users.noreply.github.com>
2023-11-14 11:26:34 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,14 +5,14 @@ struct list_node
     int value;
     list_node * next;
 };
-int len(int arr[]){
-    return (sizeof(arr)/sizeof(arr[0]));
-}
-list_node* create_linklist(int arr[],list_node *head){
+
+int main(){
+    int array[] = {1,3,5,7,9,11,13,15};
+    list_node *head = nullptr;
     list_node *current = nullptr;
-    for (int i = 0;i < len(arr);i++){
+    for (int i = 0;i < sizeof(array)/sizeof(array[0]);i++){
         list_node *node = new list_node;
-        node -> value = arr[i];
+        node -> value = array[i];
         node -> next = nullptr;
         if(head == nullptr){
             head = node;
@@ -23,13 +23,7 @@ list_node* create_linklist(int arr[],list_node *head){
             current = node;
         }  
     }
-    return head;
-}
 
-int main(){
-    int array[] = {1,3,5,7,9,11,13,15};
-    list_node *head = nullptr;
-    head = create_linklist(array,head);
     for(list_node *visitor = head; visitor != nullptr; visitor  = visitor -> next){
         std::cout << visitor -> value;
     }
